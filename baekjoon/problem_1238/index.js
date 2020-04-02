196d7aa85e56c1bcc5f0484289cb994aa7819b3f
devethan <devohno.ko@gmail.com>
2020-04-03 03:18:10 +0900

    [b1238] update result - queue 구조

--- baekjoon/problem_1238/index.js
+++ baekjoon/problem_1238/index.js
@@ -11,34 +11,62 @@ const rl = require('readline')
     [N, M, X] = input.shift().split(' ').map(Number);
 
     let time = [];
-    for (let i = 0; i < N; i++) {
-      let arr = [];
-      for (let j = 0; j < N; j++) {
-        arr.push(i === j ? 0 : Infinity);
-      }
-      time.push(arr);
+    for (let i = 0; i <= N; i++) {
+      time.push([]);
     }
 
     while (M-- > 0) {
       const [i, j, c] = input.shift().split(' ').map(Number);
-      time[i - 1][j - 1] = c;
+      time[i].push([j, c]);
     }
 
-    for (let k = 0; k < N; k++) {
-      for (let i = 0; i < N; i++) {
-        for (let j = 0; j < N; j++) {
-          if (time[i][j] > time[i][k] + time[k][j]) {
-            time[i][j] = time[i][k] + time[k][j];
-          }
-        }
+    var temp = [];
+    var result = [];
+    for (var i = 1; i <= N; i++) {
+      var arr = dijkstra(i, time);
+      temp[i] = arr;
+    }
+
+    for (var i = 1; i <= N; i++) {
+      if (i !== X) {
+        result.push(temp[i][X] + temp[X][i]);
       }
     }
 
-    let result = 0;
-    for (let i = 0; i < N; i++) {
-      let temp = time[i][X] + time[X][i];
-      if (temp > result) result = temp;
-    }
+    var max = result.reduce(function(pre, cur) {
+      return pre > cur ? pre : cur;
+    });
 
-    console.log(result);
+    console.log(max);
   });
+
+function dijkstra(start, map) {
+  var dist = [];
+  for (var i = 1; i <= N; i++) {
+    dist[i] = Infinity;
+  }
+
+  dist[start] = 0;
+  var quque = [];
+  quque.push([start, 0]);
+
+  while (quque.length) {
+    var temp = quque.shift();
+    var current = temp[0];
+    var distance = temp[1];
+
+    if (dist[current] < distance) continue;
+
+    for (var i = 0; i < map[current].length; i++) {
+      var next = map[current][i][0];
+      var nextDistance = distance + map[current][i][1];
+
+      if (nextDistance < dist[next]) {
+        dist[next] = nextDistance;
+        quque.push([next, nextDistance]);
+      }
+    }
+  }
+
+  return dist;
+}
