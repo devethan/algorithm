63f6cb51cf08ae38d07a144281348a203f07b702
devethan <devohno.ko@gmail.com>
2020-04-05 23:23:27 +0900

    [b1238] Update solution

--- baekjoon/problem_1238/index.js
+++ baekjoon/problem_1238/index.js
@@ -1,68 +1,65 @@
 let input = [];
-let N = 1000;
-let M = 10000;
-let X = N;
 const rl = require('readline')
   .createInterface(process.stdin, process.stdout)
   .on('line', function(line) {
     input.push(line.trim());
   })
   .on('close', function() {
-    [N, M, X] = input.shift().split(' ').map(Number);
+    let [N, M, X] = input.shift().split(' ').map(Number);
 
     let time = [];
-    for (let i = 0; i < N; i++) {
+    for (let i = 0; i <= N; i++) {
       time.push([]);
     }
 
     while (M-- > 0) {
       const [i, j, c] = input.shift().split(' ').map(Number);
-      time[i - 1].push([j - 1, c]);
+      time[i].push([j, c]);
     }
 
-    var temp = [];
-    for (var i = 0; i < N; i++) {
-      var arr = dijkstra(i, time);
-      temp[i] = arr;
+    function dijkstra(start) {
+      let dist = [];
+      for (let i = 1; i <= N; i++) {
+        dist[i] = Infinity;
+      }
+
+      dist[start] = 0;
+      let quque = [];
+      quque.push([start, 0]);
+
+      while (quque.length) {
+        let temp = quque.shift();
+        let current = temp[0];
+        let distance = temp[1];
+
+        if (dist[current] < distance) continue;
+
+        for (let i = 0; i < time[current].length; i++) {
+          let next = time[current][i][0];
+          let nextDistance = distance + time[current][i][1];
+
+          if (nextDistance < dist[next]) {
+            dist[next] = nextDistance;
+            quque.push([next, nextDistance]);
+          }
+        }
+      }
+
+      return dist;
+    }
+
+    let result = [];
+    for (let i = 1; i <= N; i++) {
+      let arr = dijkstra(i);
+      result[i] = arr;
     }
 
     let max = 0;
-    for (let i = 0; i < N; i++) {
-      if ( max < temp[i][X] + temp[X][i] ) {
-        max = temp[i][X] + temp[X][i];
+    for (let i = 1; i <= N; i++) {
+      if ( max < result[i][X] + result[X][i] ) {
+        max = result[i][X] + result[X][i];
       }
     }
 
     console.log(max);
   });
-
-function dijkstra(start, map) {
-  var dist = [];
-  for (var i = 0; i < N; i++) {
-    dist[i] = Infinity;
-  }
-
-  dist[start] = 0;
-  var quque = [];
-  quque.push([start, 0]);
-
-  while (quque.length) {
-    var temp = quque.shift();
-    var current = temp[0];
-    var distance = temp[1];
-
-    if (dist[current] < distance) continue;
-
-    for (var i = 0; i < map[current].length; i++) {
-      var next = map[current][i][0];
-      var nextDistance = distance + map[current][i][1];
-
-      if (nextDistance < dist[next]) {
-        dist[next] = nextDistance;
-        quque.push([next, nextDistance]);
-      }
-    }
-  }
-
-  return dist;
-}
